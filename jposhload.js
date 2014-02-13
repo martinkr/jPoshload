3537a4cec842f77a3d387e5f1c5a0950847e9a14
martin kr <github@mkrause.info>
2014-02-13 14:37:15 +0100

    version 2.0

--- jposhload.js
+++ jposhload.js
@@ -2,12 +2,12 @@
  *
  * jPoshLoad - https://github.com/martinkr/jPoshLoad
  *
- * jPoshLoad is an advanced animated preloader - think of flash-ish preloading.
+ * jPoshLoad is an advanced animated preloader - think of flash-ish preloading with multiple instances.
  *
- * @Version: 1.0.1
+ * @Version: 2.0
  *
  *
- * Copyright (c) 2011-2012 Martin Krause (jquery.public.mkrause.info)
+ * Copyright (c) 2011-2014 Martin Krause (jquery.public.mkrause.info)
  * Dual licensed under the MIT and GPL licenses.
  *
  * @author Martin Krause public@mkrause.info
@@ -16,99 +16,66 @@
  * @license GNU http://www.gnu.org/licenses/gpl-3.0.html
  *
  * @requires
- *  jQuery JavaScript Library - http://jquery.com/
- *    Copyright 2010, John Resig
- *    Dual licensed under the MIT or GPL Version 2 licenses - http://jquery.org/license
+ * 	jQuery JavaScript Library - http://jquery.com/
+ * 		Copyright 2010, John Resig
+ * 		Dual licensed under the MIT or GPL Version 2 licenses - http://jquery.org/license
  *
  * @example
-
-	jQuery(document).ready(function(){
-	 $.fn.jPoshLoad('test1', {
-		forceNoCache: true,
-		images : {
-			image1: {
-				id: 'X',
-				src: 'http://www.ipad3wallpapers.com/wp-content/uploads/2012/04/iPad-3-Wallpaper-Pattern-01.jpg'
-			},
-			image2: {
-				id: 'Y',
-				src: 'http://www.ipad3wallpapers.com/wp-content/uploads/2012/04/iPad-3-Wallpaper-Pattern-02.jpg'
-			},
-			image3: {
-				id: null,
-				src: 'http://www.ipad3wallpapers.com/wp-content/uploads/2012/04/iPad-3-Wallpaper-Pattern-03.jpg'
-			},
-			image4: {
-				id: null,
-			src: 'http://www.ipad3wallpapers.com/wp-content/uploads/2012/04/iPad-3-Wallpaper-Pattern-04.jpg'
-			},
-			image5: {
-				id: null,
-				src: 'http://www.ipad3wallpapers.com/wp-content/uploads/2012/04/iPad-3-Wallpaper-Pattern-05.jpg'
-			},
-			image6: {
-				id: null,
-				src: 'http://www.ipad3wallpapers.com/wp-content/uploads/2012/04/iPad-3-Wallpaper-Pattern-06.jpg'
-			}
-		}
-		});
-	})
+ * jQuery(document).ready(function () {
+ * 	 jQuery.fn.jPoshLoad('LoaderOne', {
+ * 		forceNoCache: true,
+ * 		images : ['src1','src2'],
+ * 		headline: 'My First Loader' // << pass custom properties
+ * 	});
+ * 	 jQuery.fn.jPoshLoad('LoaderTwo', {
+ * 		forceNoCache: false,
+ * 		images : ['src3','src4'],
+ * 		headline: 'My Second Loader' // << pass custom properties
+ * 	});
+ *
  */
 
- /* @jslint 2012-06-13*/
- /* jslint devel: true, browser: true, confusion: true, undef: true, node: false, continue: true, rhino: false, debug: false, sloppy: true, widget: false, eqeq: false, sub: true, windows: true, vars: true, evil: true, white: true, forin: true, css: true, cap: true, nomen: true, plusplus: true, maxerr: 50, indent: 4*/
+/* jshint browser:true, jquery:true, strict: false, smarttabs:true, onevar:true, undef:true, unused:true, curly:true, latedef: true, sub:true */
+/* global jQuery:true, -$:true */
 
 
-(function($) {
+;(function($) {
 
 	/**
 	* Contructor
 	* @param  {String} sIdLoader_ Loader id
 	* @param  {Object} oOptions_
 	* {
-	* 	forceNoCache: false,
-	* 	sTextHeadline : 'Loading...',
-	* 	images : {
-	* 		 id: {STRING}
-	* 		 src: {STRING}
-	* 	}
+	*	forceNoCache: false,
+ 	*	images : [{String}]
 	* }
 	* @return {Void}
 	*/
 	$.fn.jPoshLoad = function(sIdLoader_,oOptions_) {
 			jQuery('html').addClass('jPoshLoad_completed-false');
 			// build main options before element iteration
-			var _oOpts = $.extend({}, $.fn.jPoshLoad.oDefaults, oOptions_);
+			var _oOpts = $.extend({}, $.fn.jPoshLoad.oDefaults, oOptions_),
+				_$elWrapper  = jQuery($.fn.jPoshLoad.createLoaderHTML(sIdLoader_,_oOpts)),
+				_oData = jQuery(document).data('jPoshload') || {},
 
-			var _$elWrapper  = _createHTMLWrapper(sIdLoader_,_oOpts.sTextHeadline);
-			var _oData = {};
-			_oData[sIdLoader_] = {oOptions: _oOpts, _aLoad: [], _aLoaded:[],$elWrapper:_$elWrapper};
+				_aElements = oOptions_.images,
+				_bForceNoCache = oOptions_.forceNoCache || false,
+				_i
+			;
 
-			var _oElements = oOptions_.images;
-			var _sForceNoCache = oOptions_.forceNoCache || false;
-			var _oElement;
+			_oData[sIdLoader_] = {oOptions: _oOpts, iTotal: 0 , _aLoad: [], _aLoaded:[],$elWrapper:_$elWrapper};
 
-			for (_oElement in _oElements) {
-				if (!_oElements[_oElement]['id'] ) {
-					_oElements[_oElement]['id']=['jPoshload_',++$.fn.jPoshLoad.UID].join('');
-				}
-				// create status item html
-				_$elWrapper.append('<li class=""></li>');
-				_oData[sIdLoader_]._aLoad.push(_oElements[_oElement]['id']);
+			for (_i = 0; _i <_aElements.length; _i++) {
+				_oData[sIdLoader_]._aLoad.push(_aElements[_i]);
 			}
 
-			$(document).data('jPoshload',_oData);
+			_oData[sIdLoader_].iTotal = _oData[sIdLoader_]._aLoad.length;
+			_oData[sIdLoader_].iLoaded = 0;
 
-			_$elWrapper
-				.find('.jPoshLoad_counter')
-					.text(['0 / ',_$elWrapper.find('li').size()].join(''))
-				.end()
-				.find('.jPoshload_percentage')
-					.text('0%')
-				.end();
+			jQuery(document).data('jPoshload',_oData);
 
-			for (_oElement in _oElements) {
-				_addItem(sIdLoader_,_sForceNoCache,_oElements[_oElement]);
+			for (_i = 0; _i <_aElements.length; _i++) {
+				_addItem(sIdLoader_,_bForceNoCache,_aElements[_i]);
 			}
 
 	};
@@ -117,191 +84,179 @@
 	// private functions
 
 	/**
-	* Adds plugin / class specific events.
+	* Adds an item to the loading queue
 	* @private
-	* @param  {String} sIdLoader_ Completed loader's id
-	* @param {Bool} sForceNoCache_ Use a GET-Param to prevent caching images
-	* @param {Object} oElement_      Items's options
+	* @param  {String} sIdLoader_ the current loader's id
+	* @param {Bool} bForceNoCache_ Set to true if we should use a GET-Param to prevent caching images
+	* @param {String} sSrc_			The Image source
 	* @return {Void}
 	*/
-	function _addItem(sIdLoader_,sForceNoCache_,oElement_) {
+	function _addItem(sIdLoader_,bForceNoCache_,sSrc_) {
 			// handle image
-			var _sSrc = oElement_.src;
 
 			var _elImage = document.createElement('img');
-			jQuery(_elImage).bind('load.jPoshLoad',{sIdElement:oElement_.id,sIdLoader: sIdLoader_},$.fn.jPoshLoad.onLoadItem);
 
-			_elImage.src = [_sSrc,(sForceNoCache_ ? ['?',oElement_.id,'-',$.fn.jPoshLoad.UID].join('') : '' )].join('') ;
+			jQuery(_elImage).on('load.jPoshLoad',{sIdLoader: sIdLoader_},$.fn.jPoshLoad.onLoadItem);
+
+			_elImage.src = [sSrc_,(bForceNoCache_ ? ['?',++$.fn.jPoshLoad.UID].join('') : '' )].join('') ;
+
 			// execute callback for cached images
 			// opera and ie6 will not trigger the load event on those elements
 			if ( _elImage.complete || _elImage.readyState === 4 ) {
 				$.fn.jPoshLoad.onLoadItem({
 					target: _elImage,
 					data : {
-						sIdLoader:sIdLoader_,
-						sIdElement:oElement_.id
+						sIdLoader:sIdLoader_
 					}
 				});
 			}
 
 	}
 
+
 	/**
-	* Executed when all elements are completed
+	* Returns the number of images still not loaded completely
 	* @private
 	* @param  {String} sIdLoader_ Completed loader's id
-	* @param  {jQuery-Element} $elWrapper_ The loader's wrapper element
-	* @return {Void}
+	* @return {Number}	Images left to load
 	*/
-	function _doOnComplete(sIdLoader_,$elWrapper_) {
-		// if (jQuery('html').hasClass('jPoshLoad_completed-false') === false) {
-		// 	// poll for onloadItem complete
-		// 	window.setTimeout(function(){
-		// 		$.fn.jPoshLoad.onComplete(sIdLoader_);
-		// 	},100);
-		// 	return;
-		// }
-
-		if ( jQuery('html').hasClass('jPoshLoad_completed-false') === false ) {return;}
-		jQuery('html').removeClass('jPoshLoad_completed-false');
-
-		// reset or hide
-		$elWrapper_
-			.fadeOut(1000);
-			//.remove()
-			// .find('.jPoshLoad_counter')
-			// 	.text([''].join(''))
-			// .end()
-			// 	.find('.jPoshload_percentage')
-			// 	.width('0%')
-			// .end();
+	function _getStatus(sIdLoader_) {
+		var _oData = jQuery(document).data('jPoshload')[sIdLoader_];
+		return _oData.iTotal - _oData.iLoaded;
 	}
 
 
 	/**
-	* Adds plugin / class specific events.
-	* @return {HTMLElement}
+	* Returns the number of images still not loaded completely
 	* @private
-	*/
-	function _createHTMLWrapper(sId_,sTextHeadline_) {
-		return jQuery(['#',sId_].join('')).append('<div class="jPoshLoad_wrapper"><h1>'+sTextHeadline_+'</h1><span class="jPoshLoad_counter"></span><span class="jPoshload_percentage"></span><ol id="jPoshLoad_'+sId_+'"></ol></div>');
-
-	}
-
-
-
-	// public functions
-	/**
-	* onComplete callback - triggered when every element has been loaded
 	* @param  {String} sIdLoader_ Completed loader's id
-	* @return {Void}
+	* @return {Number}	Images left to load
 	*/
-	$.fn.jPoshLoad.onComplete = function (sIdLoader_) {
-		_doOnComplete(sIdLoader_,$(document).data('jPoshload')[sIdLoader_].$elWrapper);
-	};
+	function _updateState(sIdLoader_) {
+		var _oData = jQuery(document).data('jPoshload')[sIdLoader_],
+			_$elWrapper =  _oData.$elWrapper,
+			_iTotal =  _oData.iTotal,
+			_iLoaded =  _oData.iLoaded
+		;
 
-	/**
-	* Updates the status elements
-	* @param  {String} sIdLoader_ Current loader's id
-	* @return {Void}
-	*/
-	$.fn.jPoshLoad.updateStatus = function (sIdLoader_) {
-		var _$elWrapper =  $(document).data('jPoshload')[sIdLoader_].$elWrapper;
-		// var _$elWrapper  = jQuery(['#',sIdLoader_].join(''))
-		var _$elLi = _$elWrapper.find('li');
-		var _$elLiNotLoaded = _$elLi.not('.loaded');
-		_$elLiNotLoaded
-				.eq(0)
-					.addClass('loaded');
-
-		_$elLiNotLoaded = _$elWrapper.find('li').not('.loaded');
-
-		// animation and stuff
-		_$elWrapper
-
-			.find('.jPoshLoad_counter')
-				.text([_$elLi.size()-_$elLiNotLoaded.size(),' / ',_$elLi.size()].join(''))
-			.end()
-			// use css3 if  necessary
-			.find('.jPoshload_percentage')
-				.stop()
-				.animate({'width' : [ Math.floor( ( ( _$elLi.size() - _$elLiNotLoaded.size() )*100 ) / _$elLi.size() ),'% '].join('')} )
-			.end();
-
-		// everonyes finally loaded call onComplete
-		// if( $(document).data('jPoshload')[sIdLoader_]._aLoad.lenght === 0) {
-		if(_$elLiNotLoaded.size() === 0) {
-			_doOnComplete(sIdLoader_,_$elWrapper);
-		}
-	};
-
-
-	/**
-	* onLoadItem callback for the image element
-	* @param {Event} event_, DOM-event
-	* @return {Void}
-	*/
-	$.fn.jPoshLoad.onLoadItem = function(event_) {
-		var _i;
-		var _iIndex;
-		var _evtElement = jQuery(event_.target);
-
-		// update
-		var _oData = $(document).data('jPoshload');
-		var _aLoad = _oData[event_.data.sIdLoader]._aLoad;
-
-
-		_oData[event_.data.sIdLoader]._aLoaded.push(event_.data.sIdElement);
-
-		_evtElement.unbind('load.jPoshLoad');
-
-		// get current item's position in controll array
-		for (_i=0;_i<_aLoad.length; _i++) {
-			if (_aLoad[_i] == event_.data.sIdElement) {
-				_iIndex = _i;
-				break;
+		$.fn.jPoshLoad.updateStatus(
+			{
+				sIdLoader: sIdLoader_ ,
+				iTotal: _iTotal ,
+				iLoaded: _iLoaded,
+				$elWrapper:_$elWrapper
 			}
+		);
+
+		if(_iLoaded === _iTotal) {
+			$.fn.jPoshLoad.onComplete(sIdLoader_,_$elWrapper);
 		}
-
-		// remove current item
-		_oData[event_.data.sIdLoader]._aLoad =_aLoad.slice(0,_iIndex).concat( _aLoad.slice(_iIndex+1) );
-		 $(document).data('jPoshload',_oData);
-
-		_evtElement= null;
-
-		// set status
-		$.fn.jPoshLoad.updateStatus(event_.data.sIdLoader);
-
-		// notify other elements
-		jQuery('document').triggerHandler('jPoshLoad;load;element',{element:event_.target, id: event_.data.sIdElement});
-
-		return;
-	};
+	}
 
 
-	// plugin defaults
-
-	$.fn.jPoshLoad.__version = 1.1; // class version
+	// public vars
+	$.fn.jPoshLoad.__version = 2.0; // class version
 	$.fn.jPoshLoad.__class = '$.fn.jPoshLoad'; // class name
 	$.fn.jPoshLoad.UIDBase = new Date().getTime();
 	$.fn.jPoshLoad.UID = $.fn.jPoshLoad.UIDBase;
 	$.fn.jPoshLoad.sClassLoaded = 'jPoshLoad_loaded';
 
+	// public functions
 
-	// plugin defaults
+	/**
+	* onLoad-callback for every image element
+	* @param {Event} event_, DOM-event
+	* @return {Void}
+	*/
+	$.fn.jPoshLoad.onLoadItem = function(event_) {
+		var	_$element = jQuery(event_.target),
+			_oData = jQuery(document).data('jPoshload'),
+			_sIdLoader = event_.data.sIdLoader
+		;
+
+		_oData[_sIdLoader].iLoaded = _oData[_sIdLoader].iLoaded+1;
+		 jQuery(document).data('jPoshload',_oData);
+
+		_$element.off('load.jPoshLoad');
+		_$element= null;
+
+		// set status
+		_updateState(_sIdLoader);
+
+		return;
+	};
+
+	/**
+	* Returns the number of images still not loaded completely
+	* @param  {String} sIdLoader_ Completed loader's id
+	* @return {Number}	Images left to load
+	*/
+	$.fn.jPoshLoad.getStatus = function(sIdLoader_) {
+		return _getStatus(sIdLoader_);
+	};
+
+	/**
+	* Check if a specific loader is done with loading
+	* @param  {String} sIdLoader_ Completed loader's id
+	* @return {Bool}
+	*/
+	$.fn.jPoshLoad.isComplete = function(sIdLoader_) {
+		return !_getStatus(sIdLoader_);
+	};
+
+	/**
+	 * OVERRIDE THESE FUNCTIONS!
+	 */
+
+	/**
+	* Executed when all elements are completed
+	* ATTENTION! Override with your own function!
+	* @param  {String} sIdLoader_ the completed loader's id
+	* @param  {jQuery-Element} $elWrapper_ HTML-loader's wrapper element
+	* @return {Void}
+	*/
+	$.fn.jPoshLoad.onComplete = function (sIdLoader_,$elWrapper_) {
+		jQuery('html').removeClass('jPoshLoad_completed-false');
+		$elWrapper_.delay(1500).fadeOut();
+	};
+
+	/**
+	* Updates the status elements
+	* ATTENTION! Override with your own function!
+	* @param  {Object} oOptions_ the current loaders properties {sIdLoader, iTotal, iLoaded, $elWrapper}
+	* @return {Void}
+	*/
+	$.fn.jPoshLoad.updateStatus = function (oOptions_) {
+
+		// animation and stuff
+		oOptions_.$elWrapper.find('.bar')
+			.stop(true,true)
+			.animate( {'width' : [ Math.floor( (oOptions_.iLoaded / oOptions_.iTotal ) *100 ) ,'%'].join('')},1000);
+			// console.log(oOptions_.iLoaded , ' / ', oOptions_.iTotal  ,  Math.floor( (oOptions_.iLoaded / oOptions_.iTotal ) *100 )  + "%")
+	};
+
+	/**
+	* Creates HTML elements for the preloader
+	* ATTENTION! Override with your own function!
+	* @param  {String} sIdLoader_ the current loader's id
+	* @param  {Object} oOptions_ the current loader's options
+	* @return {jQuery-Element} It is important to return the loader's html wrapper. This will be stored and passed to you later (updateStatus etc).
+	*/
+	$.fn.jPoshLoad.createLoaderHTML = function (sIdLoader_,oOptions_) {
+		jQuery('body')
+			.append('<div id="html-'+sIdLoader_+'" style="padding: 5px 0 ;  background: #1e1e1e; color:#ddd; position: relative; top:0; left:0; z-index: 1000; height: 40px; width: 100%;">&nbsp;'+sIdLoader_+' - '+oOptions_.headline+'<div class="bar" style="height: 10px; width: 0%; background: red;"></div></div>');
+
+		return jQuery('#html-'+sIdLoader_);
+	};
+
+
+	// set plugin defaults
 	$.fn.jPoshLoad.oDefaults = {
 		forceNoCache: false,
-		sTextHeadline : 'Loading...',
-		images : {
-			/**
-			 id: {STRING}
-			 src: {STRING}
-			*/
-		}
-
+		images : []
 	};
-})(jQuery);
 
+})(jQuery);
 
 
 
